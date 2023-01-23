6b4dcfcec8cb2dfc3ceb1a1324944177eee25b45
jigar <jigar.k@simformsolutions.com>
2023-01-23 17:59:01 +0530

    cals fineal

--- script.js
+++ script.js
@@ -18,3 +18,43 @@ function changefontsize() {
 function isOverflown(element) {
   return element.scrollWidth > element.clientWidth;
 }
+// for on the buttons button 
+const screen = document.getElementById("myInput");
+const ans = document.getElementById("ansInput");
+let screenValue = ""; // inital value on the screen is empty
+document.getElementById("calculator").addEventListener("click", function(e) {
+  const tgt = e.target;
+  if (tgt.tagName !== "BUTTON") return;
+
+  var buttonText = tgt.innerText; //  Gets the text of that button which is clicked 
+ 
+  if (buttonText == 'x') { // since the cross X sign is * multiplication sign.
+    buttonText = '*';
+    screen.value = screenValue;
+  }
+  if (buttonText == '÷') { // since the cross X sign is * multiplication sign.
+    buttonText = '/';
+    screenValue += buttonText;
+    screen.value = screenValue;
+  } 
+  else if (buttonText == '(') {
+    buttonText = "*(";
+    screenValue += buttonText;
+    screen.value = screenValue;
+  }
+  else if (buttonText == 'C') { // for clearing the screen
+    screenValue = " ";
+    screen.value = screenValue;
+  }
+  
+  else if (buttonText == '🡄') { // for clearing last entered
+    screenValue = screenValue.slice(0,-1);
+    screen.value = screenValue;
+  } 
+  else if (buttonText == '=') { // = for calculating the arithmetic operation.
+    ans.value = eval(screenValue);
+  } else {
+    screenValue += buttonText; // for appending inputs together
+    screen.value = screenValue;
+  }
+})
